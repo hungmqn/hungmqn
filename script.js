7cfe9172d20dad8617247794c2795c7753b043b8
Hung Nguyen <hungmqn@gmail.com>
2023-12-26 21:19:40 +0700

    Update homepage

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 console.log(
   '%cWhat are you doing here buddy?',
-  'color: red; font-family:monospace; font-size: 20px;'
+  'color: green; font-family:monospace; font-size: 20px;'
 );
-
-window.onload() = {
-  
-}
